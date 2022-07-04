6f01ea031be9345663e3b9adeb54ba0cdc822927
Jikarinka <107375263+Jikarinka@users.noreply.github.com>
2022-07-04 15:52:50 +0700

    Add files via upload

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, 'Bye🖐 bot akan left!!')
+        await this.reply(m.chat, 'Bot Sudah Expired Kak,Jika Ingin Sewa Bisa Hubungi Owner Lagi😊,Bot Akan Left Bye👋')
         await this.groupLeave(m.chat)
         chats.expired = null
     }
