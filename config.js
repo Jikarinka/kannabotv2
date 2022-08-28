f310e398f6ec26da1efe1eab4c96a55ae31c24e4
lower-keys <lowerkeys9@gmail.com>
2022-08-28 17:14:22 +0700

    🇺🇦🇺🇦🇺🇦

--- config.js
+++ config.js
@@ -171,8 +171,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = '.'
-global.stickauth = `☂︎\n𝗞\na\nn\nn\na\n-\n𝗕\n𝗢\n𝗧\n✦\n\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
+global.stickpack = 'ヂマスーサトリアータマ'
+global.stickauth = `😅☝️\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
 
 global.multiplier = 38 // The higher, The harder levelup
 
